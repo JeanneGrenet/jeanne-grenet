78296d7646324b5fa3863cd5a771195b2bd47e2f
Jeanne Grenet <jeanne.grenet@gmail.com>
2025-06-25 15:42:04 +0200

    fix : footer alignment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
 
   return (
     <footer className="w-full border-t border-primary/10 py-6 mt-16 bg-secondary/20 px-4">
-      <div className="container flex flex-col items-center gap-4 md:flex-row md:justify-between">
+      <div className="container flex flex-col  gap-4 md:flex-row md:justify-between mx-auto">
         <p className="text-sm text-muted-foreground">
           © {year}{" "}
           <span className="text-primary font-medium">Jeanne Grenet</span>. All
