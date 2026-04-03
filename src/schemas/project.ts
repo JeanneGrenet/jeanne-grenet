73a829432e279d69ef7eb5afd0acbcf44a44a481
Jeanne Grenet <jeanne.grenet@gmail.com>
2026-04-03 15:21:15 +0200

    feat: add progress on project and 2 new projects

--- src/schemas/project.ts
+++ src/schemas/project.ts
@@ -1,6 +1,9 @@
 import { z, type SchemaContext } from "astro:content";
 import { zMediaImage } from "./utils";
 
+export const projectStatus = ["done", "in-progress"] as const;
+export type ProjectStatus = (typeof projectStatus)[number];
+
 export const zProject = ({ image }: SchemaContext) =>
   z.object({
     name: z.string(),
@@ -9,6 +12,7 @@ export const zProject = ({ image }: SchemaContext) =>
     technos: z.array(z.string()).optional(),
     hasDetailsPage: z.boolean().nullish(),
     link: z.string().url().nullish(),
+    status: z.enum(projectStatus).default("done"),
   });
 
 export type Project = z.infer<ReturnType<typeof zProject>>;
